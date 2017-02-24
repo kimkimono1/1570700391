299f69f3bbb3c6721d55fbdfbbd65bd630d71aec
Karn Auntachai <kim-kimono@hotmail.com>
2017-02-24 20:17:45 +0700

    10.Explosion

---
+++ 1570700391-Space Shooter/Assets/Scripts/DestroyByContact.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyByContact : MonoBehaviour {
+	public GameObject explosion;
+	public GameObject playerExplosion;
+
+	void OnTriggerEnter (Collider other)
+	{
+		if (other.gameObject.tag == "Boundary") {
+			return;
+		} else
+			Instantiate (explosion, transform.position, transform.rotation);
+		if (other.tag == "Player") {
+			Instantiate (playerExplosion, other.transform.position, other.transform.rotation);
+		}
+			Destroy (other.gameObject);
+			Destroy (gameObject);﻿
+	}
+}
