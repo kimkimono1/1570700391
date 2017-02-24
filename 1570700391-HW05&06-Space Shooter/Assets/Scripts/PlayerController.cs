5f679f2a5a0c9fa2fca7d264c998a8cc204ba9e6
Karn Auntachai <kim-kimono@hotmail.com>
2017-02-24 18:23:43 +0700

    7. Shooting shots

--- 1570700391-HW05&06-Space Shooter/Assets/Scripts/PlayerController.cs
+++ 1570700391-HW05&06-Space Shooter/Assets/Scripts/PlayerController.cs
@@ -7,26 +7,45 @@ public class Boundary
 	public float xMin, xMax, zMin, zMax;
 }
 
-public class PlayerController : MonoBehaviour {
+
+public class PlayerController : MonoBehaviour 
+{
+
 	public float speed;
 	public float tilt;
 	public Boundary boundary;
 
-	void FixedUpdate()
+	public GameObject shot;
+	public GameObject shotSpawn; 
+	public float fireRate;
+
+	private float nextFire; 
+
+	void Update () 
 	{
+		if (Input.GetButton("Fire1") && Time.time > nextFire) 
+		{
+			shot = Instantiate(shot) as GameObject;
+			nextFire = Time.time + fireRate;
+			//GameObject clone = 
+			shot.transform.position = shotSpawn.transform.position;
+
+		}
+	}
+	void FixedUpdate ()
+	{
+
 		float moveHorizontal = Input.GetAxis ("Horizontal");
 		float moveVertical = Input.GetAxis ("Vertical");
-		var movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
-		var rigidbody = GetComponent<Rigidbody> ();
 
-		rigidbody.velocity = movement * speed;
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		GetComponent<Rigidbody>().velocity = movement * speed;
 
-		rigidbody.position = new Vector3 (
-			Mathf.Clamp (rigidbody.position.x, boundary.xMin, boundary.xMax),
-			0.0f,
-			Mathf.Clamp (rigidbody.position.z, boundary.zMin, boundary.zMax)
-		);
-
-		rigidbody.rotation = Quaternion.Euler (0.0f, 0.0f, rigidbody.velocity.x * -tilt);
+		GetComponent<Rigidbody>().position = new Vector3
+			(
+				Mathf.Clamp (GetComponent<Rigidbody>().position.x, boundary.xMin, boundary.xMax), 0.0f, Mathf.Clamp (GetComponent<Rigidbody>().position.z, boundary.zMin, boundary.zMax)
+			);
+		GetComponent<Rigidbody>().rotation = Quaternion.Euler (0.0f, 0.0f, GetComponent<Rigidbody>().velocity.x * -tilt);
 	}
+
 }﻿
