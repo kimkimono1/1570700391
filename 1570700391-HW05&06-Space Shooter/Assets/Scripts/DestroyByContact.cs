08adc839b74bebdab6021815bdd7f170ca17d54d
Karn Auntachai <kim-kimono@hotmail.com>
2017-02-24 19:31:14 +0700

    9.Creating hazards

---
+++ 1570700391-HW05&06-Space Shooter/Assets/Scripts/DestroyByContact.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyByContact : MonoBehaviour {
+
+	void OnTriggerEnter (Collider other)
+	{
+		if (other.gameObject.tag == "Boundary") {
+			return;
+		} else
+			Destroy (other.gameObject);
+		Destroy (gameObject);﻿
+	}
+}
