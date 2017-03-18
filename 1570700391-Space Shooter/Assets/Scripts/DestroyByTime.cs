f4de8c061add7f80da7f3533a98a043caf4f7e46
Karn Auntachai <kim-kimono@hotmail.com>
2017-03-18 10:59:00 +0700

    12.Spawning Waves

---
+++ 1570700391-Space Shooter/Assets/Scripts/DestroyByTime.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	void onTriggerExit(Collider other)
+	{
+		Object.DestroyObject (other.gameObject);
+	}﻿
+}
